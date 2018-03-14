3756c515d9fe6b89793db7a4701c036385fdcdb7
Jude Evroh <judevroh1234@gmail.com>
2018-03-14 15:06:52 +0100

    Set up swagger for heroku

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import expressValidator from 'express-validator';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import router from './server/routes/index';
 
 
 const app = express();
+const swaggerDocument = YAML.load(`${process.cwd()}/swagger.yaml`);
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
 app.use('/', router);
-
 
 export default app;
