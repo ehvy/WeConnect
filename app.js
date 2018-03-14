e7bc5b8cace8b1ab619173767d5da80f5b5befdd
Jude Evroh <judevroh1234@gmail.com>
2018-03-14 16:59:03 +0100

    move port to app.js

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ import router from './server/routes/index';
 const app = express();
 const swaggerDocument = YAML.load(`${process.cwd()}/swagger.yaml`);
 
+const port = parseInt(process.env.PORT, 10) || 3000;
+app.set('port', port);
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
 app.use('/', router);
+
+app.listen(port, () => {
+  console.log('Server started on port 3000...');
+});
 
 export default app;
