aab8dc6c43b205ad15f2095cb30e8cd074ea70f6
ehvy <judevroh1234@gmail.com>
2018-03-20 10:09:56 -0700

    Uninstall unneeded dependencies

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,6 @@ import validateBusiness from '../middleware/businessMiddleware';
 
 const router = express.Router();
 
-
 // homepage route
 router.get('/api/v1', (req, res) => res.status(200).json({
   message: 'Welcome to WeConnect'
